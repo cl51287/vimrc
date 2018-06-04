a9bad5cc1ab3fede0ddb1bfa1ff526e69ce01cf0
cl51287 <cl51287@qq.com>
2018-06-04 14:18:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
+cp $(dirname $0)/vimrc ~/
 vim +PluginInstall +qall
 
 export TERM=xterm-256color
